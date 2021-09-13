245d1ffc5e95172764e536015a35c3f5c4012ae9
kimdonghwan96 <80185863+kimdonghwan96@users.noreply.github.com>
2021-09-13 23:50:30 +0900

    서버연동
    ㅇㅇ

--- Allergy/app/src/main/java/com/example/allergy/ResisterActivity.java
+++ Allergy/app/src/main/java/com/example/allergy/ResisterActivity.java
@@ -3,6 +3,7 @@ package com.example.allergy;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,10 +16,10 @@ public class ResisterActivity extends AppCompatActivity {
     UserInfoManager infoManager = new UserInfoManager();
     private String UserName, UserEmail, UserPwd;
     private EditText name, email, pwd, pwd_chk,brm;
-    private Button confirm, cancel, check;
+    private Button register, cancel, check;
     private boolean pwd_checked = false;
     private boolean email_checked = false;
-    private int business_registration_num;
+    private String crn;
     @Override
     protected void onCreate( Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -26,23 +27,34 @@ public class ResisterActivity extends AppCompatActivity {
         //값 찾기
         SharedPreferences sf = getSharedPreferences("sFile",MODE_PRIVATE);
         name = findViewById(R.id.resister_name);
-        email = findViewById(R.id.resister_email);
         pwd = findViewById(R.id.resister_pwd);
         pwd_chk = findViewById(R.id.resister_pwd_chk);
-        confirm = findViewById(R.id.confirm_button);
+        register = findViewById(R.id.register_button);
         brm = findViewById(R.id.business_registration_number);
 
-        confirm.setOnClickListener(new View.OnClickListener() {
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //값 저장
                 if(pwd.getText().toString().equals(pwd_chk.getText().toString())) {
-                    UserName = name.getText().toString();
+                    /*UserName = name.getText().toString();
                     UserEmail = email.getText().toString();
                     UserPwd = pwd.getText().toString();
                     infoManager.putUserInfo(UserEmail,UserPwd);
-                    business_registration_num = Integer.parseInt(brm.getText().toString());
-                    Snackbar.make(v, "회원가입 완료.", Snackbar.LENGTH_LONG).show();
+                    business_registration_num = Integer.parseInt(brm.getText().toString()); */
+                    try {
+                        String result;
+                        String id = name.getText().toString();
+                        String pw = pwd.getText().toString();
+                        String crn = brm.getText().toString();
+
+                        Register task = new Register();
+                        result = task.execute(id, pw, crn).get();
+                        Snackbar.make(v, task.receiveMsg, Snackbar.LENGTH_LONG).show();
+                    } catch (Exception e) {
+                        Log.i("DBTest", "....ERROR....!");
+                    }
+
                 } else {
                     Snackbar.make(v, "비밀번호가 다릅니다.", Snackbar.LENGTH_LONG).show();
                 }
